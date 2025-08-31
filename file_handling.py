4ed352bb83ba99291d14c2f5405760c3ac282c23
Raman Butta <raman.butta@live.in>
2025-08-31 10:09:13 +0000

    file_handling - csv functionality

--- file_handling.py
+++ file_handling.py
@@ -52,10 +52,57 @@ with open('file.txt', 'r') as f:
 with open('students.txt','w') as s:
   while True:
     name = input("Enter the student's name(Or enter END to finish)")
-    if name.lower() == 'end'':
+    if name.lower() == 'end':
       break
     s.write(name + '\n')
 
 print("Student names saved to students.txt :")
 with open('students.txt', 'r') as s:
   print(s.read())
+
+
+# 5. Create a new CSV file and write contents to it
+
+import csv
+
+# Data to write
+header = ['Name', 'Roll', 'Marks']
+rows = [
+    ['Alice', '101', '85'],
+    ['Bob', '102', '90'],
+    ['Charlie', '103', '78']
+]
+
+with open('new_students.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(header)  # write the header
+    writer.writerows(rows)   # write multiple rows
+
+
+# 6. Open the CSV file for reading
+print(f"\n The new_students.csv file is :")
+with open('new_students.csv', mode='r') as ns:
+    csv_reader = csv.reader(ns)
+
+    # Read the header
+    header = next(csv_reader)
+    print(f"Header: {header}")
+
+    # Read each row after the header
+    for row in csv_reader:
+        print(f"Row: {row}")
+
+# Open the file again for DictReader
+    print(f"\n The new_students.csv file in dictionary format is :")
+    ns.seek(0)
+    csv_reader = csv.DictReader(ns)
+    for row in csv_reader:
+        print(f"Name: {row['Name']}, Roll: {row['Roll']}, Marks: {row['Marks']}")
+
+# 7. Calculating average marks of the class
+    ns.seek(0)
+    csv_reader = csv.reader(ns)
+    next(csv_reader)  # Skip the header
+    marks = [int(row[2]) for row in csv_reader]
+    avg_marks = sum(marks)/len(marks)
+    print(f"Average marks of the class: {avg_marks}")
